ac1048ab1cd94bb19c8b8c8731d36cce32ad372e
vasu2411 <vasushah76@gmail.com>
2020-10-30 13:58:08 -0400

    Added UI

---
+++ src/Starmatch.js
@@ -0,0 +1,38 @@
+import './Starmatch.css';
+
+function Starmatch() {
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+        </div>
+        <div className="right">
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+          <button className="number">4</button>
+          <button className="number">5</button>
+          <button className="number">6</button>
+          <button className="number">7</button>
+          <button className="number">8</button>
+          <button className="number">9</button>
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
+    </div>
+  );
+}
+
+export default Starmatch;
